ba8b84544fd36232e9ec40801e842c4d37c7c52f
dmussaku <dmussaku@gmail.com>
2024-01-15 10:17:03 +0100

    Add celerybeat schedule

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 from config import CELERY_BACKEND, CELERY_BROKER
 from main import main
@@ -7,6 +8,12 @@ from main import main
 os.environ.setdefault('CELERY_CONFIG_MODULE', 'config')
 app = Celery('app', backend=CELERY_BACKEND, broker=CELERY_BROKER)
 app.config_from_envvar('CELERY_CONFIG_MODULE')
+app.conf.beat_schedule = {
+ "run-me-every-ten-seconds": {
+    "task": "app.poll_launches",
+    "schedule": crontab(minute="*/5")
+ }
+}
 
 
 @app.task
